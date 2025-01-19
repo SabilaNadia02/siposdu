4cefbe23d0c0f04a50ab2c5ae7f0573dd126eea9
SabilaNadia02 <nadiasabilala@gmail.com>
2025-01-19 22:33:50 +0700

    first commit - trying bootstrap template

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,8 @@
+<aside class="app-sidebar">
+  <nav>
+      <ul>
+          <li><a href="#"><i class="bi bi-speedometer"></i> Dashboard</a></li>
+          <li><a href="#"><i class="bi bi-gear"></i> Settings</a></li>
+      </ul>
+  </nav>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/layouts/mainheader');
 });
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+})->name('dashboard');
+
+Route::get('/profile', function () {
+    return view('pages.profile');
+})->name('profile');
