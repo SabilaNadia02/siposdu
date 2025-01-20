8ff9c9c7d2b762ae87fb1bb4d0c796c6f0f1d29a
SabilaNadia02 <nadiasabilala@gmail.com>
2025-01-20 20:06:12 +0700

    Finishing slicing layouts

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,98 +1,102 @@
-<!--begin::Sidebar-->
-<aside class="app-sidebar bg-dark text-white shadow">
-  <!--begin::Sidebar Brand-->
-  <div class="sidebar-brand py-3 px-4">
-    <!--begin::Brand Text-->
-    <span class="brand-text fw-bold fs-4 text-white">SIPOSDU ILP</span>
-    <!--end::Brand Text-->
-  </div>
-  <!--end::Sidebar Brand-->
-
-  <!--begin::Sidebar Wrapper-->
-  <div class="sidebar-wrapper">
-    <nav class="mt-3">
-      <!--begin::Sidebar Menu-->
-      <ul class="nav sidebar-menu flex-column" role="menu" data-accordion="false">
-        <!-- Utama -->
-        <li class="nav-header text-uppercase fs-6 fw-bold text-white">Utama</li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-house-door"></i>
-            <p>Dashboard</p>
+      <!--begin::Sidebar-->
+      <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+        <!--begin::Sidebar Brand-->
+        <div class="sidebar-brand">
+          <!--begin::Brand Link-->
+          <a href="./index.html" class="brand-link">
+            <!--begin::Brand Image-->
+            <img
+              src="{{ asset('adminlte') }}/dist/assets/img/AdminLTELogo.png"
+              alt="AdminLTE Logo"
+              class="brand-image opacity-75 shadow"
+            />
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">SIPOSDU ILP</span>
+            <!--end::Brand Text-->
           </a>
-        </li>
-
-        <!-- Fitur -->
-        <li class="nav-header text-uppercase fs-6 fw-bold text-white mt-3">Fitur</li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-pencil-square"></i>
-            <p>Pendaftaran</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-journal-text"></i>
-            <p>Pencatatan</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-clipboard-data"></i>
-            <p>Laporan</p>
-          </a>
-        </li>
-
-        <!-- Data Master -->
-        <li class="nav-header text-uppercase fs-6 fw-bold text-white mt-3">Data Master</li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-people-fill"></i>
-            <p>Data Kelulusan Balita</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-droplet-half"></i>
-            <p>Data Imunisasi</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-capsule-pill"></i>
-            <p>Data Vitamin</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-capsule"></i>
-            <p>Data Obat</p>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-capsule"></i>
-            <p>Data Vaksin</p>
-          </a>
-        </li>       
-        <li class="nav-item">
-          <a href="#" class="nav-link text-white">
-            <i class="nav-icon bi bi-person"></i>
-            <p>Data Pengguna</p>
-          </a>
-        </li>
-
-        <!-- Logout -->
-        <li class="nav-item mt-3">
-          <a href="#" class="nav-link text-danger">
-            <i class="nav-icon bi bi-box-arrow-right"></i>
-            <p>Keluar</p>
-          </a>
-        </li>
-      </ul>
-      <!--end::Sidebar Menu-->
-    </nav>
-  </div>
-  <!--end::Sidebar Wrapper-->
-</aside>
-<!--end::Sidebar-->
+          <!--end::Brand Link-->
+        </div>
+        <!--end::Sidebar Brand-->
+        <!--begin::Sidebar Wrapper-->
+        <div class="sidebar-wrapper">
+          <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+              class="nav sidebar-menu flex-column"
+              data-lte-toggle="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+              <li class="nav-header">MULTI LEVEL EXAMPLE</li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon bi bi-circle-fill"></i>
+                  <p>Level 1</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon bi bi-circle-fill"></i>
+                  <p>
+                    Level 1
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Level 2</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>
+                        Level 2
+                        <i class="nav-arrow bi bi-chevron-right"></i>
+                      </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">
+                          <i class="nav-icon bi bi-record-circle-fill"></i>
+                          <p>Level 3</p>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">
+                          <i class="nav-icon bi bi-record-circle-fill"></i>
+                          <p>Level 3</p>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="#" class="nav-link">
+                          <i class="nav-icon bi bi-record-circle-fill"></i>
+                          <p>Level 3</p>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Level 2</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon bi bi-circle-fill"></i>
+                  <p>Level 1</p>
+                </a>
+              </li>
+            </ul>
+            <!--end::Sidebar Menu-->
+          </nav>
+        </div>
+        <!--end::Sidebar Wrapper-->
+      </aside>
+      <!--end::Sidebar-->
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('/layouts/mainheader');
+    return view('layouts.master');
 });
-
-Route::get('/dashboard', function () {
-    return view('pages.dashboard');
-})->name('dashboard');
-
-Route::get('/profile', function () {
-    return view('pages.profile');
-})->name('profile');
